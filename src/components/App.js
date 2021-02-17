e8d1107408f2466b7ec62077b086864f3e8b40e0
Shaban <shabans@c02yn2lplvcj.fios-router.home>
2021-02-17 16:00:44 -0500

    added dispacher

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import {selectSong} from '../actions/index';
-import actions, { selectSong } from '../actions/index'
+import SongList from './SongList';
 
 const App = () => {
   return (
-    <div>App</div>
-  )
+    <div className="ui container grid">
+      <div className="ui row">
+        <div className="column eight wide">
+          <SongList />
+        </div>
+      </div>
+    </div>
+  );
 };
 export default App;
-
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,5 @@
+import {combineReducers} from 'redux';
+
 const songsReducer = () => {
   return [
     { title: 'Title1', duration: '4:05' },
@@ -7,6 +9,15 @@ const songsReducer = () => {
   ];
 };
 
-const selectedSongReducer = (selectedSong = null) => {
-  
+const selectedSongReducer = (selectedSong = null, action) => {
+  if(action.type === 'SONG_SELECTED') {
+    return action.payload;
+  }
+
+  return selectedSong;
 };
+
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer
+});
